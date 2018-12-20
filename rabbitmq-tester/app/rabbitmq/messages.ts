144ea17f502abdc44ae3937fa77231baf87888df
Jeroen Vervaeke <jeroen.vervaeke@proceedix.com>
2018-12-20 17:33:26 +0100

    FIxed isValidMessage

--- rabbitmq-tester/app/rabbitmq/messages.ts
+++ rabbitmq-tester/app/rabbitmq/messages.ts
@@ -18,8 +18,7 @@ export interface DoorOpenedMessage {
 export const isValidMessage = (message: any): message is AllMessages =>
   !(
     message == undefined ||
-    message.type !== 'open_door' ||
-    message.type !== 'door_opened' ||
+    (message.type !== 'open_door' && message.type !== 'door_opened') ||
     message.payload == undefined ||
     message.payload.office == undefined ||
     (message.type === 'open_door' && message.payload.origin == undefined)
